aa0d8055189cf3c344d895d8431c9d1c6be2541b
Matthew Dodi <matthew.dodi@gmail.com>
2019-02-18 22:59:43 -0800

    type: feat - Added styling and jsx for register.js component - handled user signup with firebase

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,8 +1,103 @@
 import React, { Component } from 'react';
+import firebase from '../../firebaseSetup';
+import { Link } from 'react-router-dom';
+import {
+  Grid,
+  Form,
+  Segment,
+  Button,
+  Header,
+  Message,
+  Icon
+} from 'semantic-ui-react';
 
 class Register extends Component {
+  state = {
+    username: '',
+    email: '',
+    password: '',
+    passwordConfirmation: ''
+  };
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleSubmit = event => {
+    if (this.isFormValid()) {
+      event.preventDefault();
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.state.email, this.state.password)
+        .then(createdUser => {
+          console.log(createdUser);
+        })
+        .catch(err => console.log(err));
+    }
+  };
+
   render() {
-    return <div>Register</div>;
+    const { username, email, password, passwordConfirmation } = this.state;
+    return (
+      <Grid textAlign='center' verticalAlign='middle' className='app'>
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Header as='h2' icon color='orange' textAlign='center'>
+            <Icon name='puzzle piece' color='orange' />
+            Register for chitChat
+          </Header>
+          <Form size='large' onSubmit={this.handleSubmit}>
+            <Segment stacked>
+              <Form.Input
+                fluid
+                name='username'
+                icon='user'
+                iconPosition='left'
+                placeholder='Username'
+                onChange={this.handleChange}
+                value={username}
+                type='text'
+              />
+              <Form.Input
+                fluid
+                name='email'
+                icon='mail'
+                iconPosition='left'
+                placeholder='Email Address'
+                onChange={this.handleChange}
+                value={email}
+                type='text'
+              />
+              <Form.Input
+                fluid
+                name='password'
+                icon='lock'
+                iconPosition='left'
+                placeholder='Password'
+                onChange={this.handleChange}
+                value={password}
+                type='password'
+              />
+              <Form.Input
+                fluid
+                name='passwordConfirmation'
+                icon='repeat'
+                iconPosition='left'
+                placeholder='Password Confirmation'
+                onChange={this.handleChange}
+                value={passwordConfirmation}
+                type='password'
+              />
+              <Button color='orange' fluid size='large'>
+                Submit
+              </Button>
+            </Segment>
+          </Form>
+          <Message>
+            Already a user? <Link to='/login'>Login</Link>
+          </Message>
+        </Grid.Column>
+      </Grid>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import './index.css';
+import 'semantic-ui-css/semantic.min.css';
 import App from './components/App';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
@@ -10,9 +10,9 @@ import * as serviceWorker from './serviceWorker';
 const Root = () => (
   <Router>
     <Switch>
-      <Route path='/' component={App} />
-      <Route path='/login' component={Login} />
       <Route path='/register' component={Register} />
+      <Route path='/login' component={Login} />
+      <Route path='/' component={App} />
     </Switch>
   </Router>
 );
