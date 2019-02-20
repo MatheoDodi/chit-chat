05749a77a851c4992fa91d81836bd1066d169d74
Matthew Dodi <matthew.dodi@gmail.com>
2019-02-19 22:38:36 -0800

    type: feat - Added onAuthStateChanged listener that redirects user to main page after sign in or sign up

--- src/index.js
+++ src/index.js
@@ -1,23 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  withRouter
+} from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
+import firebase from './firebaseSetup';
 import App from './components/App';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 import * as serviceWorker from './serviceWorker';
 
-const Root = () => (
-  <Router>
-    <Switch>
-      <Route path='/register' component={Register} />
-      <Route path='/login' component={Login} />
-      <Route path='/' component={App} />
-    </Switch>
-  </Router>
-);
+class Root extends Component {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.props.history.push('/');
+      }
+    });
+  }
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+  render() {
+    return (
+      <Switch>
+        <Route path='/register' component={Register} />
+        <Route path='/login' component={Login} />
+        <Route path='/' component={App} />
+      </Switch>
+    );
+  }
+}
+
+const RootWithRouter = withRouter(Root);
+
+ReactDOM.render(
+  <Router>
+    <RootWithRouter />
+  </Router>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
