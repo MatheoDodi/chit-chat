ebdb7be825eb9bf0c4bc821d487ccca1c66df9fb
Matthew Dodi <matthew.dodi@gmail.com>
2019-02-21 22:59:15 -0800

    type: added first channel to database

--- src/components/SidePanel/Channels.js
+++ src/components/SidePanel/Channels.js
@@ -1,19 +1,59 @@
 import React, { Component, Fragment } from 'react';
 import { Menu, Icon, Modal, Form, Input, Button } from 'semantic-ui-react';
+import firebase from '../../firebaseSetup';
 
 class Channels extends Component {
   state = {
+    user: this.props.currentUser,
     channels: [],
     showModal: false,
     channelName: '',
-    channelDetails: ''
+    channelDetails: '',
+    channelsRef: firebase.database().ref('channels')
+  };
+
+  addChannel = () => {
+    const { user, channelsRef, channelName, channelDetails } = this.state;
+
+    const key = channelsRef.push().key;
+    const newChannel = {
+      id: key,
+      name: channelName,
+      details: channelDetails,
+      createdBy: {
+        name: user.displayName,
+        avatar: user.photoURL
+      }
+    };
+
+    channelsRef
+      .child(key)
+      .update(newChannel)
+      .then(() => {
+        this.setState({ channelname: '', channelDetails: '' });
+        this.handleCloseModal();
+        console.log('Channel Added');
+      })
+      .catch(err => console.log(err));
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    if (this.isFormValid(this.state)) {
+      this.addChannel();
+    }
   };
 
   handleChange = event => {
     this.setState({ [event.target.name]: event.target.value });
   };
 
+  isFormValid = ({ channelName, channelDetails }) =>
+    channelName && channelDetails;
+
   handleCloseModal = () => this.setState({ showModal: false });
+
+  handleOpenModal = () => this.setState({ showModal: true });
 
   render() {
     const { channels, showModal } = this.state;
@@ -25,10 +65,7 @@ class Channels extends Component {
               <Icon name='exchange' /> Channels
             </span>{' '}
             ({channels.length}){' '}
-            <Icon
-              onClick={() => this.setState({ showModal: true })}
-              name='add'
-            />
+            <Icon onClick={this.handleOpenModal} name='add' />
           </Menu.Item>
         </Menu.Menu>
         <Modal basic open={showModal} onClose={this.handleCloseModal}>
@@ -54,7 +91,7 @@ class Channels extends Component {
             </Form>
           </Modal.Content>
           <Modal.Actions>
-            <Button color='green' inverted>
+            <Button color='green' onClick={this.handleSubmit} inverted>
               <Icon name='checkmark' /> Add
             </Button>
             <Button color='red' onClick={this.handleCloseModal} inverted>
--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -15,7 +15,7 @@ class SidePanel extends Component {
         style={{ background: '#4c3c4c', fontSize: '1.2rem' }}
       >
         <UserPanel currentUser={currentUser} />
-        <Channels />
+        <Channels currentUser={currentUser} />
       </Menu>
     );
   }
