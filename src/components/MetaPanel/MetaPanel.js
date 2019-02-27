13fab5fd5f8ee3617437fdf0b16731b89c29ff97
Matthew Dodi <matthew.dodi@gmail.com>
2019-02-26 19:22:26 -0800

    type: feat

--- src/components/MetaPanel/MetaPanel.js
+++ src/components/MetaPanel/MetaPanel.js
@@ -1,8 +1,66 @@
 import React, { Component } from 'react';
+import { Segment, Header, Accordion, Icon } from 'semantic-ui-react';
 
 class MetaPanel extends Component {
+  state = {
+    activeIndex: 0
+  };
+
+  setActiveIndex = (event, titleProps) => {
+    const { index } = titleProps;
+    const { activeIndex } = this.state;
+    const newIndex = activeIndex === index ? -1 : index;
+    this.setState({ activeIndex: newIndex });
+  };
+
   render() {
-    return <div>MetaPanel</div>;
+    const { activeIndex } = this.state;
+
+    return (
+      <Segment style={{ marginRight: '2em' }}>
+        <Header as='h3' attached='top'>
+          About # Channel
+        </Header>
+        <Accordion styled attatched='true'>
+          <Accordion.Title
+            active={activeIndex === 0}
+            index={0}
+            onClick={this.setActiveIndex}
+          >
+            <Icon name='dropdown' />
+            <Icon name='info' />
+            ChannelDetails
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === 0}>
+            Details
+          </Accordion.Content>
+          <Accordion.Title
+            active={activeIndex === 1}
+            index={1}
+            onClick={this.setActiveIndex}
+          >
+            <Icon name='dropdown' />
+            <Icon name='user circle' />
+            Top posters
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === 1}>
+            posters
+          </Accordion.Content>
+          <Accordion.Title
+            active={activeIndex === 2}
+            index={2}
+            onClick={this.setActiveIndex}
+          >
+            <Icon name='dropdown' />
+            <Icon name='pencil alternate' />
+            ChannelDetails
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === 2}>
+            creator
+          </Accordion.Content>
+        </Accordion>
+      </Segment>
+    );
   }
 }
 
